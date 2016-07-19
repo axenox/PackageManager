50055e261c397c0ed5380a4cb97c256ad33f2ec8
Andrej Kabachnik <andrej_ka@gmx.de>
2016-07-19 09:44:43 +0200

    NEW ComposerRequire action

---
+++ Actions/ComposerRequire.php
@@ -0,0 +1,40 @@
+<?php namespace axenox\PackageManager\Actions;
+
+use kabachello\ComposerAPI\ComposerAPI;
+use exface\Core\Exceptions\ActionRuntimeException;
+
+/**
+ * This action runs one or more selected test steps
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class ComposerRequire extends AbstractComposerAction {
+	
+	protected function init(){
+		$this->set_icon_name('install');
+	}	
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \axenox\PackageManager\Actions\AbstractComposerAction::perform_composer_action()
+	 */
+	protected function perform_composer_action(ComposerAPI $composer){
+		if (!$this->get_input_data_sheet()->get_meta_object()->is('axenox.PackageManager.PACKAGE_INSTALLED')){
+			throw new ActionRuntimeException('Wrong input object for action "' . $this->get_alias_with_namespace() . '" - "' . $this->get_input_data_sheet()->get_meta_object()->get_alias_with_namespace() . '"! This action requires input data based based on "axenox.PackageManager.PACKAGE_INSTALLED"!');
+		}
+				
+		$packages = array();
+		foreach ($this->get_input_data_sheet()->get_rows() as $nr => $row){
+			if (!isset($row['name']) || !$row['name']){
+				throw new ActionRuntimeException('Missing package name in row ' . $nr . ' of input data for action "' . $this->get_alias_with_namespace() . '"!');
+			}
+			$packages[] = $row['name'] . ($row['version'] ? ':' . $row['version'] : '');
+		}
+		
+		return $composer->require($packages);
+	}
+	
+}
+?>
