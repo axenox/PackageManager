81ae8a2011bd49375c29ad71a521662dd98517d5
Andrej Kabachnik <andrej_ka@gmx.de>
2016-07-20 13:18:22 +0200

    NEW action ExportAppModel

---
+++ Actions/ExportAppModel.php
@@ -0,0 +1,116 @@
+<?php namespace axenox\PackageManager\Actions;
+
+use exface\Core\Interfaces\Actions\ActionInterface;
+use exface\Core\Exceptions\AppNotFoundError;
+use exface\Core\CommonLogic\AbstractApp;
+use axenox\PackageManager\PackageManagerApp;
+use exface\Core\CommonLogic\Model\Object;
+use exface\Core\CommonLogic\AbstractAction;
+use exface\Core\CommonLogic\NameResolver;
+use exface\Core\Exceptions\ActionRuntimeException;
+
+/**
+ * This Action saves alle elements of the meta model assotiated with an app as JSON files in the Model subfolder of the current 
+ * installations folder of this app.
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class ExportAppModel extends AbstractAction {
+	
+	private $export_to_path_relative = null;
+	
+	protected function init(){
+		$this->set_icon_name('export');
+		$this->set_input_rows_min(0);
+		$this->set_input_rows_max(null);
+	}	
+	
+	protected function perform(){
+		$apps = $this->get_input_apps_data_sheet();
+		
+		$exported_counter = 0;
+		foreach ($apps->get_rows() as $row){
+			try {
+				$app = $this->get_workbench()->get_app($row['ALIAS']);
+			} catch (AppNotFoundError $e){
+				$workbench = $this->get_workbench();
+				$name_resolver = NameResolver::create_from_string($row['ALIAS'], NameResolver::OBJECT_TYPE_APP, $workbench);
+				$this->get_app()->create_app_folder($name_resolver);
+				$app = $this->get_workbench()->get_app($row['ALIAS']);
+			}
+			$this->export_model($app);
+			$exported_counter++;
+		}
+			
+		// Save the result and output a message for the user
+		$this->set_result('');
+		$this->set_result_message('Exported model files for ' . $exported_counter . ' apps to app-folders in "' . $this->get_export_to_path_relative() . '".');
+		
+		return;
+	}
+	
+	/**
+	 * 
+	 * @throws ActionRuntimeException
+	 * @return \exface\Core\Interfaces\DataSheets\DataSheetInterface
+	 */
+	protected function get_input_apps_data_sheet(){
+		if ($this->get_input_data_sheet()
+				&& !$this->get_input_data_sheet()->is_empty()
+				&& !$this->get_input_data_sheet()->get_meta_object()->is('exface.Core.APP')){
+					throw new ActionRuntimeException('Action "' . $this->get_alias() . '" exprects an exface.Core.APP as input, "' . $this->get_input_data_sheet()->get_meta_object()->get_alias_with_namespace() . '" given instead!');
+		}
+		
+		$apps = $this->get_input_data_sheet()->copy();
+		$apps->get_columns()->add_from_expression('ALIAS');
+		if (!$apps->is_up_to_date()){
+			if (!$apps->is_empty()){
+				$apps->add_filter_from_column_values($apps->get_uid_column()->get_values(false));
+			}
+			$apps->data_read();
+		}
+		return $apps;
+	}
+	
+	protected function export_model(AbstractApp $app){
+		$this->get_app()->filemanager()->mkdir($this->get_app()->get_path_to_app_absolute($app, $this->get_export_to_path_relative()) . DIRECTORY_SEPARATOR . PackageManagerApp::FOLDER_NAME_MODEL);
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.APP'), 'UID');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.APP_CONFIG'), 'APP');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.OBJECT'), 'APP');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.OBJECT_BEHAVIORS'), 'OBJECT__APP');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.ATTRIBUTE'), 'OBJECT__APP');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.DATASRC'), 'APP');
+		$this->export_model_file($app, $this->get_workbench()->model()->get_object('ExFace.Core.CONNECTION'), 'APP');
+	}
+	
+	protected function export_model_file(AbstractApp $app, Object $object, $app_filter_attribute_alias){
+		/** @var $ds \exface\Core\CommonLogic\DataSheets\DataSheet */
+		$ds = $this->get_workbench()->data()->create_data_sheet($object);
+		foreach ($object->get_attribute_group('~ALL')->get_attributes() as $attr){
+			$ds->get_columns()->add_from_expression($attr->get_alias());
+		}
+		$ds->add_filter_from_string($app_filter_attribute_alias, $app->get_uid());
+		$ds->get_sorters()->add_from_string($object->get_uid_alias(), 'ASC');
+		$ds->data_read();
+		$this->get_app()->filemanager()->dumpFile($this->get_app()->get_path_to_app_absolute($app, $this->get_export_to_path_relative()) . DIRECTORY_SEPARATOR . PackageManagerApp::FOLDER_NAME_MODEL . DIRECTORY_SEPARATOR . $object->get_alias() . '.json', $ds->to_uxon());
+	}
+	
+	public function get_export_to_path_relative() {
+		return $this->export_to_path_relative;
+	}
+	
+	public function set_export_to_path_relative($value) {
+		$this->export_to_path_relative = $value;
+		return $this;
+	}   
+	
+	/**
+	 * @return PackageManagerApp
+	 * @see \exface\Core\Interfaces\Actions\ActionInterface::get_app()
+	 */
+	public function get_app(){
+		return parent::get_app();
+	}
+}
+?>
--- PackageManagerApp.php
+++ PackageManagerApp.php
@@ -1,6 +1,10 @@
 <?php
 namespace axenox\PackageManager;
 
+use exface\Core\Interfaces\AppInterface;
+use exface\Core\Interfaces\NameResolverInterface;
+use exface\Core\Factories\AppFactory;
+
 class PackageManagerApp extends \exface\Core\CommonLogic\AbstractApp {
 	
 	const FOLDER_NAME_MODEL = 'Model';
@@ -9,5 +13,80 @@ class PackageManagerApp extends \exface\Core\CommonLogic\AbstractApp {
 		return $this->get_workbench()->filemanager();
 	}
 	
+	public function create_app_folder(NameResolverInterface $name_resolver){
+		
+		// Make sure the vendor folder exists
+		$app_vendor_folder = $this->filemanager()->get_path_to_vendor_folder() . DIRECTORY_SEPARATOR . $name_resolver->get_vendor();
+		if (!is_dir($app_vendor_folder)){
+			mkdir($app_vendor_folder);
+		}
+		
+		// Create the app folder
+		$app_folder = $app_vendor_folder . DIRECTORY_SEPARATOR . $name_resolver->get_alias();
+		if (!is_dir($app_folder)){
+			mkdir($app_folder);
+		}
+		
+		// Make sure, the app class exists
+		if (!class_exists($name_resolver->get_class_name_with_namespace())){
+			$this->create_app_class($name_resolver);
+		}
+		$app = AppFactory::create($name_resolver);
+				
+		$this->create_composer_json($app);
+	}
+	
+	protected function create_app_class(NameResolverInterface $name_resolver){
+		$class_name = $name_resolver->get_alias() . 'App';
+		$class_namespace = substr($name_resolver->get_class_namespace(), 1);
+		$file_path = $this->filemanager()->get_path_to_vendor_folder() . $name_resolver->get_class_directory() . DIRECTORY_SEPARATOR . $class_name . '.php';
+		$file_contents = <<<PHP
+<?php namespace {$class_namespace};
+				
+class {$class_name} extends \\exface\\Core\\CommonLogic\\AbstractApp {
+	
+}	
+PHP;
+
+		$this->filemanager()->dumpFile($file_path, $file_contents);
+	}
+	
+	protected function create_composer_json(AppInterface $app){
+		$file_path = $this->get_path_to_app_absolute($app) . DIRECTORY_SEPARATOR . 'composer.json';
+		if (!file_exists($file_path)){
+			$json = array(
+					"name" 		=> $app->get_vendor() . '/' . str_replace($app->get_vendor() . $this->get_workbench()->get_config_value('namespace_separator') , '', $app->get_alias_with_namespace()),
+					"require" 	=> array(
+							"exface/core" => "~0.1"
+					),
+					"extra" 	=> array(
+							"app_uid" => $app->get_uid()
+					)
+			);
+			$this->filemanager()->dumpFile($file_path, json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+		}
+		return $this;
+	}
+	
+	/**
+	 * Returns the path to the
+	 * @param AbstractApp $app
+	 * @return string
+	 */
+	public function get_path_to_app_relative(AppInterface $app = null, $base_path = '') {
+		$path = '';
+		if (!$base_path){
+			if ($this->get_configuration_value('PATH_TO_AUTHORED_PACKAGES')){
+				$path = $this->get_app()->get_configuration_value('PATH_TO_AUTHORED_PACKAGES') . DIRECTORY_SEPARATOR;
+			} else {
+				$path = 'vendor' . DIRECTORY_SEPARATOR;
+			}
+		}
+		return $path . ($app ? $app->get_vendor() . DIRECTORY_SEPARATOR . str_replace($app->get_vendor() . $this->get_workbench()->get_config_value('namespace_separator'), '', $app->get_alias()) : '');
+	}
+	
+	public function get_path_to_app_absolute(AppInterface $app = null, $base_path = ''){
+		return $this->get_workbench()->get_installation_path() . DIRECTORY_SEPARATOR  . $this->get_path_to_app_relative($app, $base_path);
+	}
 }
 ?>
