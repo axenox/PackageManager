8234bfea7c57ead73fc774ef164bb7b066374c9e
Andrej Kabachnik <andrej_ka@gmx.de>
2016-10-05 15:50:38 +0200

    FIX added connection and data source to model

---
+++ ComposerJson.php
@@ -0,0 +1,10 @@
+<?php namespace axenox\PackageManager;
+
+use exface\Core\CommonLogic\UxonObject;
+
+class ComposerJeson extends UxonObject {
+	
+	// TODO Add special methods like add_script() here. Extending UxonObject will enable future utilization of json schema, etc.
+	
+}
+?>
