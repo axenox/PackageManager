85e39c994d3be90826fd73c2b0a7ea84ec8faf77
Andrej Kabachnik <andrej_ka@gmx.de>
2018-05-24 15:48:20 +0200

    FIX error texts

--- MetaModelInstaller.php
+++ MetaModelInstaller.php
@@ -314,7 +314,7 @@ class MetaModelInstaller extends AbstractAppInstaller
                         $result .= ($result ? "; " : "") . $data_sheet->getMetaObject()->getName() . " - " . $counter;
                     }
                 } catch (\Throwable $e) {
-                    throw new InstallerRuntimeError($this, 'Failed to install model sheet for "' . $data_sheet->getMetaObject()->getAliasWithNamespace() . '": see details below.', null, $e);
+                    throw new InstallerRuntimeError($this, 'Failed to install model sheet for "' . $data_sheet->getMetaObject()->getAliasWithNamespace() . '": ' . $e->getMessage(), null, $e);
                 }
             }
             
