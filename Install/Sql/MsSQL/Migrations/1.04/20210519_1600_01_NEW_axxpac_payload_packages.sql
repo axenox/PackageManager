6444068432bdab3fe9d9a56c08c188983261d693
ralf.mulansky <ralf.mulansky@CPX-0CQZ83U363X.dir.svc.accenture.com>
2021-06-02 11:45:27 +0200

    NEW added MsSql migration for payload packages table

---
+++ Install/Sql/MsSQL/Migrations/1.04/20210519_1600_01_NEW_axxpac_payload_packages.sql
@@ -0,0 +1,19 @@
+-- UP
+IF OBJECT_ID('dbo.axxpac_payload_packages', 'U') IS NULL
+CREATE TABLE [dbo].[axxpac_payload_packages] (
+  [oid] [binary](16) NOT NULL,
+  [created_on] [datetime2] NOT NULL,
+  [modified_on] [datetime2] NOT NULL,
+  [created_by_user_oid] [binary](16) DEFAULT NULL,
+  [modified_by_user_oid] [binary](16) DEFAULT NULL,
+  [type] [nvarchar](50) NOT NULL,
+  [name] [nvarchar](128) NOT NULL,
+  [url] [nvarchar](200) NOT NULL,
+  [version] [nvarchar](5) DEFAULT NULL,
+  CONSTRAINT PK_axxpac_payload_packages_oid PRIMARY KEY (oid)
+);
+	
+-- DOWN
+
+IF OBJECT_ID('dbo.axxpac_payload_packages', 'U') IS NOT NULL 	
+DROP TABLE IF EXISTS [dbo].[axxpac_payload_packages];
